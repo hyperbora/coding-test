6b0febfa5f70f2b408cdfb3a64ab870c1a73289b
hyperbora <hyperbora@gmail.com>
2021-08-21 23:48:39 +0900

    edit

--- leetcode.com/longest_palindromic_substring.py
+++ leetcode.com/longest_palindromic_substring.py
@@ -1,36 +1,23 @@
+"""
+https://leetcode.com/problems/longest-palindromic-substring/
+"""
 import unittest
 
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        start = 0
-        end = start
-        mv = memoryview(s.encode())
-        s_len = len(mv)
-        max_length = 1
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+                left -= 1
+                right += 1
+            return s[left + 1:right - 1]
 
-        i = 0
-        while s_len - i > max_length and i < s_len:
-            j = i + max_length + 1
-            while j - i > max_length and j <= s_len:
-                cur_length = j - i
-                if cur_length > max_length and cur_length <= s_len:
-                    half = cur_length // 2
-                    if cur_length % 2 == 0:
-                        head = mv[i:i + half]
-                    else:
-                        head = mv[i:i + half + 1]
-                    tail = mv[j-1: i + half - 1: -1]
-                    if head == tail:
-                        max_length = cur_length
-                        start = i
-                        end = j
-                j += 1
-            i += 1
-        rst = mv[start:end].tobytes().decode()
-        if rst == '':
-            return s[0]
-        return rst
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        result = ''
+        for i in range(len(s) - 1):
+            result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
+        return result
 
 
 class TestSolution(unittest.TestCase):

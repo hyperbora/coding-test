d0fb1b3b5549bbaf21510d882bb645e0548e2ea5
hyperbora <hyperbora@gmail.com>
2021-08-22 10:32:07 +0900

    add comment

--- leetcode.com/two_pointers/trapping_rain_water.py
+++ leetcode.com/two_pointers/trapping_rain_water.py
@@ -6,6 +6,11 @@ import unittest
 
 
 class Solution:
+    """
+    리스트에서 가장 높은 막대를 찾는다. 막대는 높고 낮음에 무관하게
+    전체 부피에 영향을 주지 않으며 왼쪽, 오른쪽을 가르는 장벽 역할을 한다.
+    """
+
     def trap(self, height: List[int]) -> int:
         if not height:
             return 0
@@ -18,6 +23,12 @@ class Solution:
             left_max, right_max = max(height[left], left_max), max(
                 height[right], right_max)
 
+            # 최대 높이의 막대까지 각각 좌우 기둥 최대 높이 left_max, right_max가
+            # 현재 높이와의 차이만큼 물 높이 volume을 더해 간다.
+            # 적어도 낮은 쪽은 그만큼 항상 채워질 것이기 때문에, 좌우 어느 쪽이든 낮은 쪽은 높은 쪽을 향해서
+            # 포인터가 가운데로 이동한다.
+            # 오른쪽이 크면 left += 1로 왼쪽이 이동, 반대면 right -= 1로 오른쪽이 이동한다.
+            # 결국 최대 높이 지점까지 좌우 포인터가 서로 만나게 된다.
             if left_max <= right_max:
                 volume += left_max - height[left]
                 left += 1

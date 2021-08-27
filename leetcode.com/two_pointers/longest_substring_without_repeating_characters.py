424cd9bca6407e1bbcf5aced8928356ff7166fcf
hyperbora <hyperbora@gmail.com>
2021-08-27 13:46:11 +0900

    edit

--- leetcode.com/two_pointers/longest_substring_without_repeating_characters.py
+++ leetcode.com/two_pointers/longest_substring_without_repeating_characters.py
@@ -1,21 +1,25 @@
+"""
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
 import unittest
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        rst = 0
-        i = 0
-        length = len(s)
-        while i + rst < length and i < length:
-            j = i + rst + 1
-            if j <= length:
-                if len(s[i:j]) == len(set(s[i:j])):
-                    rst += 1
-                else:
-                    i += 1
+        used = {}
+        max_length = start = 0
+        for index, char in enumerate(s):
+            # 이미 등장했던 문자라면 start 위치 갱신
+            if char in used and start <= used[char]:
+                start = used[char] + 1
             else:
-                i += 1
-        return rst
+                # 최대 부분 문자열 길이 갱신
+                max_length = max(max_length, index - start + 1)
+
+            # 현재 문자의 위치 삽입
+            used[char] = index
+
+        return max_length
 
 
 class TestSolution(unittest.TestCase):

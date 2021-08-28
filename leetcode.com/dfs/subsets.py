98ca3385ecf9d01204ed58ee136386d5f1c3773c
hyperbora <hyperbora@gmail.com>
2021-08-28 20:16:44 +0900

    edit

--- leetcode.com/dfs/subsets.py
+++ leetcode.com/dfs/subsets.py
@@ -26,8 +26,8 @@ class Solution:
         def test_result(self):
             s = Solution()
             self.assertListEqual(
-                s.subsets([1, 2, 3], [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]))
-            self.assertListEqual(s.subsets([0], [[], [0]]))
+                s.subsets([1, 2, 3]), [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]])
+            self.assertListEqual(s.subsets([0]), [[], [0]])
 
 
 if __name__ == "__main__":

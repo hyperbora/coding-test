ae82ffeaacdebfefcc01d07cb6ecd38bb26ef3f7
hyperbora <hyperbora@gmail.com>
2021-08-30 13:32:41 +0900

    add file

---
+++ leetcode.com/tree/convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+정렬된 배열을 높이 균형(Height Balanced) 이진 탐색 트리로 변환
+"""
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        # 분할 정복으로 이진 검색 결과 트리 구성
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return node

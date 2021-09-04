da8cce99e01d905b272ebf4f3d45512075d22c06
hyperbora <hyperbora@gmail.com>
2021-09-04 19:45:21 +0900

    edit file

--- programmers.co.kr/news_clustering.py
+++ programmers.co.kr/news_clustering.py
@@ -3,15 +3,18 @@ https://programmers.co.kr/learn/courses/30/lessons/17677
 [1차] 뉴스 클러스터링
 """
 from collections import Counter
+import collections
+import unittest
+import re
 
 
 def solution(str1, str2):
     str1 = str1.lower()
     str2 = str2.lower()
     cnt_1 = Counter([str1[i:i+2]
-                    for i in range(0, len(str1) - 1) if str1[i:i+2].isalpha()])
+                     for i in range(0, len(str1) - 1) if str1[i:i+2].isalpha()])
     cnt_2 = Counter([str2[i:i+2]
-                    for i in range(0, len(str2) - 1) if str2[i:i+2].isalpha()])
+                     for i in range(0, len(str2) - 1) if str2[i:i+2].isalpha()])
     intersection = []
     union = []
     for key in cnt_1:
@@ -31,24 +34,43 @@ def solution(str1, str2):
     return int(result * 65536)
 
 
-"""
-str1	str2	answer
-FRANCE	french	16384
-handshake	shake hands	65536
-aa1+aa2	AAAA12	43690
-E=M*C^2	e=m*c^2	65536
-"""
+def solution2(str1, str2):
+    # 두 글자씩 끊어서 다중집합 구성
+    str1s = [
+        str1[i:i + 2].lower()
+        for i in range(len(str1) - 1)
+        if re.findall('[a-z]{2}', str1[i:i + 2].lower())
+    ]
+    str2s = [
+        str2[i:i + 2].lower()
+        for i in range(len(str2) - 1)
+        if re.findall('[a-z]{2}', str2[i:i + 2].lower())
+    ]
+
+    # 교집합 계산
+    intersection = sum((collections.Counter(str1s) &
+                        collections.Counter(str2s)).values())
+    # 합집합 계산
+    union = sum((collections.Counter(str1s) |
+                 collections.Counter(str2s)).values())
+
+    # 자카드 유사도 계산
+    jaccard_sim = 1 if union == 0 else intersection / union
+    return int(jaccard_sim * 65536)
+
+
+class TestSolution(unittest.TestCase):
+    def test_result(self):
+        self.assertEqual(solution("FRANCE", "french"), 16384)
+        self.assertEqual(solution("handshake", "shake hands"), 65536)
+        self.assertEqual(solution("aa1+aa2", "AAAA12"), 43690)
+        self.assertEqual(solution("E=M*C^2", "e=m*c^2"), 65536)
+
+        self.assertEqual(solution2("FRANCE", "french"), 16384)
+        self.assertEqual(solution2("handshake", "shake hands"), 65536)
+        self.assertEqual(solution2("aa1+aa2", "AAAA12"), 43690)
+        self.assertEqual(solution2("E=M*C^2", "e=m*c^2"), 65536)
+
 
 if __name__ == "__main__":
-    str1 = "FRANCE"
-    str2 = "french"
-    print(solution(str1, str2))
-    str1 = "handshake"
-    str2 = "shake hands"
-    print(solution(str1, str2))
-    str1 = "aa1+aa2"
-    str2 = "AAAA12"
-    print(solution(str1, str2))
-    str1 = "E=M*C^2"
-    str2 = "e=m*c^2"
-    print(solution(str1, str2))
+    unittest.main()
